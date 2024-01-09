2ff85c3e6c6eebefd0995bb5c24606201871b701
KacperDeja <81015531+KacperDeja@users.noreply.github.com>
2024-01-09 14:40:31 +0100

    resolve conflicts

--- src/Components/Auth/HandleRegisterLogic.js
+++ src/Components/Auth/HandleRegisterLogic.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const isExistingCompany = async (formData) => {
   const ApiUrl = process.env.REACT_APP_URL;
-<<<<<<< HEAD
-
-=======
->>>>>>> 12db01ea35740363d8379826ad8ae882089d89a3
   try {
     const { companies } = await axios.get(ApiUrl + "api/Company/GetAll");
     return companies.filter((Firm) => Firm.Name === formData.Name).length > 0;
@@ -15,10 +11,6 @@ const isExistingCompany = async (formData) => {
 };
 export const HandleRegisterCompany = async (formData) => {
   const ApiUrl = process.env.REACT_APP_URL;
-<<<<<<< HEAD
-
-=======
->>>>>>> 12db01ea35740363d8379826ad8ae882089d89a3
   try {
 
     if (await isExistingCompany(formData)) {
