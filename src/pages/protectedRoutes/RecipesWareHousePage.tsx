72ae5ada444f69e487794a353cb5e26098f6ed69
Szymon Czap <127615744+SzymonCzap@users.noreply.github.com>
2024-02-08 15:00:08 +0100

    feat: display recipes and recipes details

---
+++ src/pages/protectedRoutes/RecipesWareHousePage.tsx
@@ -0,0 +1,9 @@
+import RecipesWareHouse from '@/features/warehouse/recipes/RecipesWareHouse';
+import React from 'react'
+
+const RecipesWareHousePage = () => {
+  return (
+    <RecipesWareHouse />
+  )
+}
+export default RecipesWareHousePage;
